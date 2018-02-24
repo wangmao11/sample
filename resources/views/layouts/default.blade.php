444b75aabdb99ec53d5df9e55c22edb6e280e43e
vagrant <vagrant@homestead>
2018-02-24 08:07:24 +0000

    finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,17 @@
 <html>
   <head>
     <title>@yield('title', 'Sample')</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
-    @yield('content')
+  @include('layouts._header')
+  <div class="container">
+      <div class="clo-md 10 col-offset-1">
+          @yield('content')
+          @include('layouts._footer')
+      </div>
+  </div>
+
   </body>
 </html>
