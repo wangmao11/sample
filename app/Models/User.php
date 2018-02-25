d9594837fa4c7e25487ebae7c53ab7bf59b56b61
vagrant <vagrant@homestead>
2018-02-25 00:14:44 +0000

    new file

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models\User  ;
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -9,7 +9,7 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    protected $table = 'users'
+    protected $table = 'users';
 
     /**
      * The attributes that are mass assignable.
